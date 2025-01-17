b44976924e548bb97f6c4eb1215a322b21339b0a
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-01-17 22:16:01 +0530

    Update Text_Editor.java

--- src/Text_Editor.java
+++ src/Text_Editor.java
@@ -1,8 +1,3 @@
-//public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Hello world!");
-//    }
-//}
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
